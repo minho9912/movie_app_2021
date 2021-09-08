9e8c7204da1101cfd9434d69af7f9ad76bf12a3b
choiminho <pogni811@naver.com>
2021-09-08 12:50:41 +0900

    Delete Potato component Potato 컴포넌트를 App.js 내부에서 선언하고 사용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!!!!!</h1>
+      <h1>Hello React!!!!</h1>
       <Potato />
     </div>
   );
