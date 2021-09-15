9e242c5774415536ac20391872b04683e0e5f6fb
choiminho <pogni811@naver.com>
2021-09-15 12:49:40 +0900

    9월 15일 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
+
+const foodLike = [
+  {
+    name: "chikin",
+    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg",
+  },
+  {
+    name: "ham",
+    image: "https://imagesm.cj.net/images/brand/spam/img_cont1.png",
+  },
+];
 function App() {
   return (
     <div className="App">
       <h1>Hello React!!!!</h1>
-      <Potato />
+      {foodLike.map(renderFood)}
     </div>
   );
 }
-function Potato() {
-  return <h3>I love potato</h3>;
+function renderFood(dish) {
+  return <Food name={dish.name} picture={dish.image} />;
 }
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I love {name}</h3>
+      <img src={picture}></img>
+    </div>
+  );
+}
+
 export default App;
